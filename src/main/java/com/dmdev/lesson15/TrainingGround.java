ef9a2a8c59714a10c61d4ff8b269ef3038117bdb
Viner108 <andrukovichl@mail.ru>
2023-05-14 20:29:20 +0300

    video 12(Принципы ООП) commit

--- src/main/java/com/dmdev/lesson15/TrainingGround.java
+++ src/main/java/com/dmdev/lesson15/TrainingGround.java
@@ -5,12 +5,12 @@ public class TrainingGround {
         Hero warrior=new Warrior("Валерия",15,60);
         Hero mage=new Mage("Гарри Поттер", 10,40);
         Hero archer=new Archer("Робингуд", 5,45);
-        Enemy enemy=new Enemy("Буратино", 100, 25);
+        Enemy enemy=new Enemy("Буратино", 50, 25);
         attackEnemy(enemy,warrior,mage,archer);
     }
     public static void attackEnemy(Enemy enemy,Hero...heroes){
             for (Hero hero:heroes) {
-                while (hero.isAlive()) {
+                while (hero.isAlive()&& enemy.isAlive()) {
                 if (enemy.isAlive() && hero.isAlive()) {
                     hero.attackEnemy(enemy);
                     if (enemy.isAlive() && hero.isAlive()) {
