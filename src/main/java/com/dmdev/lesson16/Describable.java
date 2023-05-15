688d9d8d1882b589f57a7ea4bcf782814b8b5fc4
Viner108 <andrukovichl@mail.ru>
2023-05-15 20:31:16 +0300

    video 14(Принципы ООП) commit

---
+++ src/main/java/com/dmdev/lesson16/Describable.java
@@ -0,0 +1,5 @@
+package com.dmdev.lesson16;
+
+public interface Describable {
+  String getDescription();
+}
---
+++ src/main/java/com/dmdev/lesson16/ProcessorType.java
@@ -0,0 +1,20 @@
+package com.dmdev.lesson16;
+
+public enum ProcessorType implements Describable{
+    BIT_32("bit-32"),
+    BIT_64("bit-64");
+    private String name;
+
+    ProcessorType(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    @Override
+    public String getDescription(){
+        return name+" description";
+    }
+}
