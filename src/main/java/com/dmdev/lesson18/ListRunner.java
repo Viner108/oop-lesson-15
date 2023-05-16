e41dd14b185da7c7bf4e1f041b578637f00673df
Viner108 <andrukovichl@mail.ru>
2023-05-16 20:27:48 +0300

    video 1(Genetics) commit

---
+++ src/main/java/com/dmdev/lesson18/ListRunner.java
@@ -0,0 +1,15 @@
+package com.dmdev.lesson18;
+
+import com.dmdev.lesson15.Archer;
+
+public class ListRunner {
+    public static void main(String[] args) {
+        List<String> list = new List<>(10);
+        list.add("String1");
+        list.add("String2");
+        list.add("String3");
+
+        String element = (String) list.get(1);
+        System.out.println(element);
+    }
+}
